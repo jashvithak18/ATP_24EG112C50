bd36753d18e8f689e761286b7d057852ca75ebaf
jashvithak18 <24eg112c50@anurag.edu.in>
2026-03-05 22:06:40 +0530

    Create 2.js

---
+++ week1/2.js
@@ -0,0 +1,12 @@
+//2.big number in 3 numbers
+//2
+let n3=67;
+if(n1>n2 && n1>n3){
+    console.log(`${n1} is greater than ${n2} and ${n3}`)
+}
+else if(n2>n1 && n2>n3){
+    console.log(`${n2} is greater than ${n1} and ${n3}`)
+}
+else{
+    console.log(`${n3} is greater than ${n1} and ${n2}`)
+}

2bebe7a1ccea1381e8137f5c715298bc1c89dac9
jashvithak18 <24eg112c50@anurag.edu.in>
2026-03-05 22:10:20 +0530

    Update fun1.js

--- week1/fun1.js
+++ week1/fun1.js
@@ -16,14 +16,4 @@ console.log("big number is",result)
 
 
 
-//function that receives an array & search element as args and returns the index of that search element in array.It should return "not found" when element is not found
-let test2=function(arr,search){
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]===search){
-            return i;
-        }
-    }
-    return "not found";
-}
-let result2=test2([90,78,65,98],65)
-console.log("index is",result2)
+

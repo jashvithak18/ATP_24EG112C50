b2d7e60682e7bb7dba42f3e558e913d179eea974
jashvithak18 <24eg112c50@anurag.edu.in>
2026-03-28 11:12:03 +0530

    Add Navbar component with basic structure

---
+++ Week5/react-1/src/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <div className="navbar">
+      <h2>LOGO</h2>
+      <div>
+        <span>Home</span>
+        <span>Signup</span>
+        <span>Login</span>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;

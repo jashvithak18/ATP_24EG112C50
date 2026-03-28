6e85915dfaad3aee92ef9278cc911d6fec6db223
jashvithak18 <24eg112c50@anurag.edu.in>
2026-03-28 11:09:06 +0530

    Refactor App component to include Navbar and Footer

--- Week5/react-1/src/App.jsx
+++ Week5/react-1/src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
-import Product from '../components/Product.jsx'
-//function component
-function App(){
- 
 
-  //state
-  return(
-    <div className="m-20">
-  </div>
-  )
+import Navbar from "./Navbar";
+import UsersList from "./UsersList";
+import Footer from "./Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <UsersList />
+      <Footer />
+    </>
+  );
 }
+
 export default App;
